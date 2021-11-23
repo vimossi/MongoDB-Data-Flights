270144195f2895df154c488dd5383428c6593408
vimossi <vini_agg@hotmail.com>
2021-11-23 09:52:08 -0400

    corrige req 9 e 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,9 +1,1 @@
-db.voos.find({ "empresa.nome": "GOL", ano: 2017 }, 
-  { 
-    _id: 0,
-    vooId: 1,
-    "empresa.nome": 1,
-    "aeroportoOrigem.nome": 1,
-    "aeroportoDestino.nome": 1,
-    mes: 1,
-    ano: 1 }).limit(10);
+db.voos.find({ ano: { $gte: 2017, $lte: 2018 } }).count();
